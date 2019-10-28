83141f54d7caabbb6bc2a3c738ee9a9194064630
Señor Saddam <es4saddam@gmail.com>
2019-10-28 19:07:15 +0530

    Complete tour of hero tutorial from angular.io

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes/heroes.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'detail/:id', component: HeroDetailComponent},
+  {path: 'heroes', component : HeroesComponent},
+  {path: 'dashboard', component : DashboardComponent}
+];
 
-const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
